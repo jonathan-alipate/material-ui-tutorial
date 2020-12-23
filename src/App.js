85d162e1ea384115505ca310731d2b5266850981
Jonathan Alipate <j.alipate@gmail.com>
2020-12-23 16:57:23 +1300

    addedd discard button and buttongroup

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button 
-        startIcon={<SaveIcon />}
-        size="small" 
-        style={{}} 
-        onClick={() => console.log('howdy')} 
-        href="#" 
-        variant='contained' 
-        color="primary">
-          Hello World
-        </Button>
+        <ButtonGroup>
+          <Button
+            endIcon={<SaveIcon />}
+            size="large"
+            onClick={() => console.log("howdy")}
+            href="#"
+            variant="contained"
+            color="primary"
+          >
+            Save
+          </Button>
+          <Button
+            endIcon={<DeleteIcon />}
+            size="large"
+            onClick={() => console.log("howdy")}
+            href="#"
+            variant="contained"
+            color="secondary"
+          >
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-
     </div>
   );
 }
